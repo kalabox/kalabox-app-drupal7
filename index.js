4eb0413d21f209af7ef1e5fa08f2bdb4110ed0d9
Mike Pirog <mike@kalamuna.com>
2015-04-24 20:04:11 -0700

    Use new org

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ module.exports = function(kbox) {
   kbox.create.add('drupal7', {
     option: {
       name: 'name',
+      weight: -99,
       task: {
         name: 'name',
         alias: 'n',
@@ -26,18 +27,23 @@ module.exports = function(kbox) {
         description: 'The name of your app.',
       },
       properties: {
-        message: 'Name this app: '.rainbow,
+        message: 'Name this app'.rainbow,
         required: true,
         type: 'string',
         validator: /^[a-z0-9 ]+$/i,
-        warning: 'Answer must be alphanumeric.',
+        warning: 'App name must be alphanumeric.',
         default: 'My Drupal 7 App',
         before: function(value) { return _.kebabCase(value); }
+      },
+      conf: {
+        type: 'global',
+        key: 'appName'
       }
     }
   });
 
-  //require('./node_modules/kalabox-plugin-dbenv/create.js')(kbox);
+  require('./node_modules/kalabox-plugin-drush/create.js')(kbox);
+  require('./node_modules/kalabox-plugin-git/create.js')(kbox);
 
   // Task to create kalabox apps
   kbox.tasks.add(function(task) {
